ccb73b631f46a08dc2c5bf880f115cb325df90d6
Suruchi-Supekar <91071766+Suruchi-Supekar@users.noreply.github.com>
2022-12-14 20:24:05 +0530

    Delete Q1.ts

--- Q1.ts
+++
@@ -1,9 +0,0 @@
-
-let func=(str1:string,str2:string,str3:string,str4:string)=>
-{
-    console.log("The string is :"+str1+", Length of string is:"+str1.length);
-    console.log("The string is :"+str2+",Length of string is:"+str2.length);
-    console.log("The string is :"+str3+", Length of string is:"+str3.length);
-    console.log("The string is :"+str4+", Length of string is:"+str4.length);
-};
-func("suruchi","aditi","anaya","siddhi");
