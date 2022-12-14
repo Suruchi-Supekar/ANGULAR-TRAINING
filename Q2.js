efe398a3d96e758901ffd73e96608ff6d451da7f
Suruchi-Supekar <91071766+Suruchi-Supekar@users.noreply.github.com>
2022-12-14 20:24:16 +0530

    Delete Q2.js

--- Q2.js
+++
@@ -1,7 +0,0 @@
-var arr = [3, 8, 5, 0, 99];
-var mysort = function (arr) {
-    var result = arr.sort(function (a, b) { return a - b; });
-    console.log(result);
-};
-// console.log(mysort([3,8,5,0,99]));
-mysort(arr);
