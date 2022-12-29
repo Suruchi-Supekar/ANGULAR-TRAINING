1a65ec6c3f0bc07d615191f52c31e827123d28cf
Suruchi-Supekar <91071766+Suruchi-Supekar@users.noreply.github.com>
2022-12-30 00:20:38 +0530

    Add files via upload

---
+++ AngMaterialAssign3-src/src/app/myservice.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyserviceService {
+
+  constructor(public http:HttpClient) { }
+
+  employeeSave(data:any)
+  {
+    return this.http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=> {
+      return res
+    }))
+  }
+}
---
+++ AngMaterialAssign3-src/src/app/register/register.component.ts
@@ -0,0 +1,58 @@
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
+import { MyserviceService } from '../myservice.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  constructor(private fb:FormBuilder,private cs:MyserviceService){}
+
+  hobbies:string[]=['dancing','singing','acting','sketching'];
+  
+  colors:ChipColor[]=
+  [
+    {name:'dancing',color:undefined},
+    {name:'singing',color:'primary'},
+    {name:'acting',color:'warn'},
+    {name:'sketching',color:'accent'}
+  ]
+  empform!:FormGroup;
+
+  hide=true;
+  ngOnInit(): void {
+
+    // this.cs.employeeGetData().subscribe(list =>{
+    //   this.emp=list
+    //})
+    this.empform = this.fb.group({
+      fullName:['', [Validators.required,Validators.minLength(6),Validators.maxLength(32)]],
+      email:['', [ Validators.required, Validators.email]],
+      password:['', [Validators.required,Validators.minLength(6),Validators.maxLength(12),Validators.pattern("^[a-zA-Z0-9_.-]*$")
+    ]],
+      gender:['', [Validators.required]],
+      dob:[''],
+      mob:['',[Validators.required,Validators.pattern("@^[0-9]{10}$")]],
+      skills:[''],
+      userName:['',[Validators.required]]
+
+    })
+    
+  }
+
+  onSubmit(){
+    this.cs.employeeSave(this.empform.value).subscribe();
+    window.alert("Data Saved!!!");
+    window.location.reload();
+   }
+  
+
+}
+export interface ChipColor
+{
+  name:string;
+  color:ThemePalette;
+}
